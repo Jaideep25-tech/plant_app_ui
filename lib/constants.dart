6932ff34c82765fdc62148b902fa83c85648216e
Jaideep Singh <79747022+Jaideep25-tech@users.noreply.github.com>
2022-02-04 19:09:32 -0800

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-// Colors that we use in our app
+// Colors that i use in app
 const kPrimaryColor = Color(0xFF0C9869);
 const kTextColor = Color(0xFF3C4046);
 const kBackgroundColor = Color(0xFFF9F8FD);
